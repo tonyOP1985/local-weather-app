ad6edae8e0f723dbb72ba5a0a42f1240ddc1f8ce
tonyOP1985 <toliverpaull@gmail.com>
2017-12-08 16:18:48 -0500

    removed duplicated files

--- server/weather.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-axios.get('https://api.darksky.net/forecast/eac25f3d3f4fdd8401944180915a2d6e/37.8267,-122.4233')
-	.then((response) => {
-			console.log(response)
-	})
-	.catch((error) => {
-		console.log(error)
-	})
